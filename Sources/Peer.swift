6bd61a13e7983f8260238c880229eb3903a8b7e0
nya <home@cathand.org>
2020-07-07 13:48:48 +0900

    Update Peer.swift

--- Sources/Peer.swift
+++ Sources/Peer.swift
@@ -11,8 +11,8 @@ import MultipeerConnectivity
 /// Class containing peerID and session state
 public class Peer {
 
-    var peerID: MCPeerID
-    var state: MCSessionState
+    public var peerID: MCPeerID
+    public var state: MCSessionState
 
     init(peerID: MCPeerID, state: MCSessionState) {
         self.peerID = peerID
